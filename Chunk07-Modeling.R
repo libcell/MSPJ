f8b540bfe9e1bb5bc9068e402fccc8429d9df14a
Bo Lee <libcell@cqu.edu.cn>
2020-06-07 21:39:54 +0800

    Update Chunk07-Modeling.R

--- Chunk07-Modeling.R
+++ Chunk07-Modeling.R
@@ -35,6 +35,31 @@ rm(seq.matrix)
 # size.max: the maximum sample size in each group. 
 # ord.gene: which gene you focused on. 
 
+
+# Data Split
+# load the libraries
+library(caret)
+library(klaR)
+# load the iris dataset
+data(iris)
+# define an 80%/20% train/test split of the dataset
+split=0.80
+trainIndex <- createDataPartition(iris$Species, p=split, list=FALSE)
+data_train <- iris[ trainIndex,]
+data_test <- iris[-trainIndex,]
+# train a naive bayes model
+model <- NaiveBayes(Species~., data=data_train)
+# make predictions
+x_test <- data_test[, 1:4]
+y_test <- data_test[, 5]
+predictions <- predict(model, x_test)
+# summarize results
+confusionMatrix(predictions$class, y_test)
+
+
+
+
+
 if (!require("coin")) install.packages("coin")
 
 library(coin)
@@ -119,25 +144,7 @@ table(p[1:500] < 0.05)
 
 hist(input$g1, breaks = 10)
 
-# Data Split
-# load the libraries
-library(caret)
-library(klaR)
-# load the iris dataset
-data(iris)
-# define an 80%/20% train/test split of the dataset
-split=0.80
-trainIndex <- createDataPartition(iris$Species, p=split, list=FALSE)
-data_train <- iris[ trainIndex,]
-data_test <- iris[-trainIndex,]
-# train a naive bayes model
-model <- NaiveBayes(Species~., data=data_train)
-# make predictions
-x_test <- data_test[, 1:4]
-y_test <- data_test[, 5]
-predictions <- predict(model, x_test)
-# summarize results
-confusionMatrix(predictions$class, y_test)
+
 
 
 
