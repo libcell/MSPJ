65a55901390f83b2256c9d6d75210f34ac243937
Bo Lee <libcell@cqu.edu.cn>
2020-06-07 21:37:26 +0800

    Update Chunk07-Modeling.R

--- Chunk07-Modeling.R
+++ Chunk07-Modeling.R
@@ -119,56 +119,25 @@ table(p[1:500] < 0.05)
 
 hist(input$g1, breaks = 10)
 
-
-# load the library
+# Data Split
+# load the libraries
 library(caret)
+library(klaR)
 # load the iris dataset
 data(iris)
-# define training control
-train_control <- trainControl(method="boot", number=100)
-# train the model
-model <- train(Species~., data=iris, trControl=train_control, method="nb")
+# define an 80%/20% train/test split of the dataset
+split=0.80
+trainIndex <- createDataPartition(iris$Species, p=split, list=FALSE)
+data_train <- iris[ trainIndex,]
+data_test <- iris[-trainIndex,]
+# train a naive bayes model
+model <- NaiveBayes(Species~., data=data_train)
+# make predictions
+x_test <- data_test[, 1:4]
+y_test <- data_test[, 5]
+predictions <- predict(model, x_test)
 # summarize results
-print(model)
-
-
-# load the library
-library(caret)
-# load the iris dataset
-data(iris)
-# define training control
-train_control <- trainControl(method="cv", number=10)
-# fix the parameters of the algorithm
-grid <- expand.grid(.fL=c(0), .usekernel=c(FALSE))
-# train the model
-model <- train(Species~., data=iris, trControl=train_control, method="nb", tuneGrid=grid)
-# summarize results
-print(model)
-
-
-# load the library
-library(caret)
-# load the iris dataset
-data(iris)
-# define training control
-train_control <- trainControl(method="repeatedcv", number=10, repeats=3)
-# train the model
-model <- train(Species~., data=iris, trControl=train_control, method="nb")
-# summarize results
-print(model)
-
-# load the library
-library(caret)
-# load the iris dataset
-data(iris)
-# define training control
-train_control <- trainControl(method="LOOCV")
-# train the model
-model <- train(Species~., data=iris, trControl=train_control, method="nb")
-# summarize results
-print(model)
-
-
+confusionMatrix(predictions$class, y_test)
 
 
 
